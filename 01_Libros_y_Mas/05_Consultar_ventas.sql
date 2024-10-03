ffb709c17f7decb9040100cca1ea7216b305b4d4
VictorCancino <juanvictorcancino@gmail.com>
2024-10-03 13:04:21 -0600

    Se agregó una versión de query que uno usa alias sino que referencia directamente a las columnas de las tablas

--- 01_Libros_y_Mas/05_Consultar_ventas.sql
+++ 01_Libros_y_Mas/05_Consultar_ventas.sql
@@ -1,4 +1,10 @@
-
-SELECT v.id_venta, l.titulo, v.cantidad, v.fecha 
-FROM  ventas v
+---Usando alias---
+SELECT v.id_venta, l.titulo, v.cantidad, v.fecha
+FROM ventas v
 JOIN libros l ON v.id_libro = l.id_libro;
+
+---Referenciando directamente la columna---
+
+SELECT ventas.id_venta, libros.titulo, ventas.cantidad, ventas.fecha 
+FROM ventas
+JOIN libros ON ventas.id_libro = libros.id_libro;
