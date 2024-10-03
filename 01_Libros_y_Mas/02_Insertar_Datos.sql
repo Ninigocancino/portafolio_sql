51dcf103215faee14b98d12d78b38c61d07f57dc
VictorCancino <juanvictorcancino@gmail.com>
2024-10-03 12:16:33 -0600

    Se guardo la query que inserta datos en el inventario de libros desde el fichero '02_insertar_datos.sql'

---
+++ 01_Libros_y_Mas/02_Insertar_Datos.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO libros(titulo,autor,precio,cantidad)
+VALUES ('Cien años de soledad', 'Gabriel García Márquez', 200, 50),
+       ('Originales', 'Adam Grant', 450, 20),
+       ('Sapiens, de Animales a Dioses', 'Yuval Noah Harari', 850, 35);
