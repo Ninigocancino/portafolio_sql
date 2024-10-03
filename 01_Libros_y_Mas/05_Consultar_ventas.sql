ec9e1d4e4644e01dfef124d090f706fc92e9ef01
VictorCancino <juanvictorcancino@gmail.com>
2024-10-03 12:49:00 -0600

    Se corrigió la consulta del número total de ventas

--- 01_Libros_y_Mas/05_Consultar_ventas.sql
+++ 01_Libros_y_Mas/05_Consultar_ventas.sql
@@ -1,4 +1,4 @@
 
-SELECT ventas.id_venta, libros.titulo, ventas.cantidad, ventas.fecha 
-FROM ventas
-JOIN libros libros ON ventas.id_libro = libro.id_libro;
+SELECT v.id_venta, l.titulo, v.cantidad, v.fecha 
+FROM  ventas v
+JOIN libros l ON v.id_libro = l.id_libro;
