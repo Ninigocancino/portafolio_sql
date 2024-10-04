e3ef5bded0fb6b68c59b37611646873aaf5cbf60
VictorCancino <juanvictorcancino@gmail.com>
2024-10-04 11:38:20 -0600

    Se agregarón registros a la tabla 'mascotas'

---
+++ 02_clinica_veterinaria/02_insertar_datos.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO mascotas (nombre, especie, edad, raza, nombre_duenio, telefono_cont)
+VALUES ('Firulais', 'Perro', 'Pastor Alemán', 5, 'Juan Pérez', '555-1234'),
+       ('Michi', 'Gato', 'Siamés', 3, 'Ana López', '555-5678'),
+       ('Bobby', 'Perro', 'Labrador', 2, 'Carlos Ruiz', '555-8765');
