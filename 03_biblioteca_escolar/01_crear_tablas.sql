8fe905352ae7b4b9f86ae00b4558684329f77c3f
VictorCancino <juanvictorcancino@gmail.com>
2024-10-12 17:53:20 -0600

    Se creó la tabla prestamos en la dase de datos del proyecto desde el Query 03 en el fichero '01_crear_tablas.sql'

--- 03_biblioteca_escolar/01_crear_tablas.sql
+++ 03_biblioteca_escolar/01_crear_tablas.sql
@@ -12,5 +12,17 @@ CREATE TABLE IF NOT EXISTS estudiantes(
     id_estudiante INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL,
     grado INTEGER NOT NULL,
-    grupo TEXT NOT NULL,
+    grupo TEXT NOT NULL
+);
+
+
+--Query 03: Crear tabla prestamos
+CREATE TABLE IF NOT EXISTS prestamos(
+    id_prestamos INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_libro INTEGER,
+    id_estudiante INTEGER,
+    fecha_de_prestamo TEXTO NOT NULL,
+    fecha_de_devolucion TEXTO NOT NULL,
+    FOREIGN KEY (id_libro) REFERENCES libros(id_libros),
+    FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id_estudiante)
 );
