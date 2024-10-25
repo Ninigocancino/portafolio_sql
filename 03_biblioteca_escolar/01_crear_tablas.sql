6f5390631840210cfeb5cf3c13f8f872a376f3e1
VictorCancino <juanvictorcancino@gmail.com>
2024-10-24 21:55:32 -0600

    Se creó la tabla 'prestamos' en la base de datos 'db_biblioteca.db' desde la query: 'Crear tabla de estudiantes' ubicada en el fichero '01_crear_tablas.sql'

--- 03_biblioteca_escolar/01_crear_tablas.sql
+++ 03_biblioteca_escolar/01_crear_tablas.sql
@@ -14,3 +14,14 @@ CREATE TABLE IF NOT EXISTS estudiantes (
     grado INTEGER NOT NULL,
     grupo TEXT NOT NULL
 );
+
+--Crear tabla de préstamos
+CREATE TABLE IF NOT EXISTS prestamos(
+    id_prestamo INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_libro INTEGER,
+    id_estudiante INTEGER,
+    fecha_prestamos TEXT NOT NULL,
+    fecha_devolucion TEXT,
+    FOREIGN KEY (id_libro) REFERENCES libros(id_libro),
+    FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id_estudiante)
+);
