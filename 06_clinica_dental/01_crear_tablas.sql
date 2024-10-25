ebf5b9183624aa68bc032700c2b2bb2873f44210
VictorCancino <juanvictorcancino@gmail.com>
2024-10-25 09:06:37 -0600

    Se creó la tabla 'pacientes' en la base de datos 'db_sonrisa_saludable.db' desde la query: 'Crear tabla de pacientes' ubicada en el fichero '01_crear_tablas.sql' del directorio '06_clinica_dental'

---
+++ 06_clinica_dental/01_crear_tablas.sql
@@ -0,0 +1,8 @@
+--Crear tabla de pacientes
+CREATE TABLE IF NOT EXISTS pacientes(
+    id_paciente INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    fecha_nacimiento TEXT,
+    telefono TEXT,
+    email TEXT
+);
