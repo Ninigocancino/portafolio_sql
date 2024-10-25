a952b4e6d62e277f7fdde68671c3f6d9439214c1
VictorCancino <juanvictorcancino@gmail.com>
2024-10-24 21:08:18 -0600

    Se eliminarón archivos no necesarios

--- 05_Tienda_electronica/01_crear_tabla.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS productos(
-    id_producto INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    marca TEXT NOT NULL,
-    precio REAL NOT NULL,
-    stock INTEGER NOT NULL
-);
--- 06_clinica_dental/01_crear_tablas.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS pacientes(
-    id_paciente INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    fecha_nacimiento TEXT,
-    telefono TEXT,
-    email TEXT
-);
-
