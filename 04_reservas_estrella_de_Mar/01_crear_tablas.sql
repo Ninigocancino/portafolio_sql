2649b5cf61d39b55bd3f362ac66d9a135bdbc027
VictorCancino <juanvictorcancino@gmail.com>
2024-10-24 22:30:03 -0600

    Se creó la tabla 'habitaciones' en la base de datos 'db_hotel_EM.db' desde la query: 'Crear tabla habitaciones' ubicada en el fichero '01_crear_tablas.sql'

---
+++ 04_reservas_estrella_de_Mar/01_crear_tablas.sql
@@ -0,0 +1,8 @@
+--Crear tabla de habitaciones
+CREATE TABLE IF NOT EXISTS habitaciones (
+    id_habitaciones INTEGER PRIMARY KEY AUTOINCREMENT,
+    numero_habitacion INTEGER NOT NULL,
+    tipo TEXT NOT NULL,
+    precio_noche REAL NOT NULL,
+    estado TEXT NOT NULL
+);
