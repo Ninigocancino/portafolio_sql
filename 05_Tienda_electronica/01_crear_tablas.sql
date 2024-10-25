8b6b9884c7ad3b656eb85556b346f5c492b7792c
VictorCancino <juanvictorcancino@gmail.com>
2024-10-25 08:44:00 -0600

    Se creó la tabla 'ventas' en la base de datos 'deb_electro_max' usando la query: 'Crear tabla de ventas', ubicada en el fichero '01_crear_tablas.sql' en el directorio del proyecto

--- 05_Tienda_electronica/01_crear_tablas.sql
+++ 05_Tienda_electronica/01_crear_tablas.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS productos(
     precio REAL NOT NULL,
     stock INTEGER NOT NULL
 );
+
+--Crear tabla de ventas 
+CREATE TABLE IF NOT EXISTS ventas(
+    id_venta INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_producto INTEGER,
+    cantidad INTEGER NOT NULL,
+    fecha TEXT NOT NULL,
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+);
