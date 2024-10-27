20c9d5fc3314ba99a772b27a6d59ac55ae1bae52
VictorCancino <juanvictorcancino@gmail.com>
2024-10-26 19:22:37 -0600

    Se agregarón datos a la tabla 'estudiantes'

--- 03_biblioteca_escolar/02_insertar_datos.sql
+++ 03_biblioteca_escolar/02_insertar_datos.sql
@@ -3,3 +3,9 @@ INSERT INTO libros(titulo,autor,genero,anio_publicacion)
 VALUES ('Don Quijote de la mancah', 'Miguel de Cervantes', 'Novela', 1605),
 ('La Iliada', 'Homero', 'Epico', -750),
 ('Cálculo de una variable', 'James Stewart','Educativo', 2015);
+
+--Insertar estudiantes
+INSERT INTO estudiantes (nombre,grado,grupo)
+VALUES ('Luis García',10,'A'),
+('María Fernandez',11,'B'),
+('Carlos López', 12, 'C');
