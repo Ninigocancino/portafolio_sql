7dba1ab7286d3027e0dac9d165ab177cca896671
VictorCancino <juanvictorcancino@gmail.com>
2024-10-26 19:18:19 -0600

    Se agregarón datos a la tabla 'libros'

---
+++ 03_biblioteca_escolar/02_insertar_datos.sql
@@ -0,0 +1,5 @@
+--Insertar libros
+INSERT INTO libros(titulo,autor,genero,anio_publicacion)
+VALUES ('Don Quijote de la mancah', 'Miguel de Cervantes', 'Novela', 1605),
+('La Iliada', 'Homero', 'Epico', -750),
+('Cálculo de una variable', 'James Stewart','Educativo', 2015);
