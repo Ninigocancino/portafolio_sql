31ba6b16ce6e686f9437d61fb4e92aad1de217ee
VictorCancino <juanvictorcancino@gmail.com>
2024-10-26 19:58:20 -0600

    Se agregarón datos a la tabla 'productos'

---
+++ 05_Tienda_electronica/02_insertar_datos.sql
@@ -0,0 +1,5 @@
+--Insertar Productos
+INSERT INTO productos(nombre,marca,precio,stock)
+VALUES('Televisor 4K', 'Samsung',700.00,10),
+('Laptop','Dell',1200.00,5),
+('Smartphone', 'Apple', 999.00,15);
