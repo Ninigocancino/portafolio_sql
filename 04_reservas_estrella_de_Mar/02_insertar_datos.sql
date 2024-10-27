ebcfbbed7a48e0f1e503f6bbcfd9506c9a8ff83f
VictorCancino <juanvictorcancino@gmail.com>
2024-10-26 19:36:20 -0600

    Se agregarón datos a la tabla 'cleinetes'

--- 04_reservas_estrella_de_Mar/02_insertar_datos.sql
+++ 04_reservas_estrella_de_Mar/02_insertar_datos.sql
@@ -3,3 +3,9 @@ INSERT INTO habitaciones(numero_habitacion, tipo, precio_noche,estado)
 VALUES (101, 'Sencilla', 50.00, 'Diponible'),
 (102, 'Doble', 80.00,'Disponible'),
 (103, 'Suite',150.00, 'Disponible');
+
+
+--Insertar clientes
+INSERT INTO clientes (nombre, telefono, email)
+VALUES ('Ana Torres', '555-1234', 'ana@example.com'),
+('Pedro Jimémez', '555-5678', 'pedro@example.com');
