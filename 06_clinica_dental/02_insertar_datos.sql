98df5d8945575489384961f6910e659ad78d00cb
VictorCancino <juanvictorcancino@gmail.com>
2024-10-26 20:11:44 -0600

    Se agregarón datos a la tabla 'tratamientos'

--- 06_clinica_dental/02_insertar_datos.sql
+++ 06_clinica_dental/02_insertar_datos.sql
@@ -2,3 +2,8 @@
 INSERT INTO pacientes(nombre,fecha_nacimiento,telefono,email)
 VALUES ('Laura Martinez', '1985-06-15', '555-9876','laura@example.com'),
 ('José Ramírez', '1990-09-22','555-6543','jose@example.com');
+
+--Insertar tratamientos
+INSERT INTO tratamientos (nombre,descripcion, costo)
+VALUES ('Limpieza dental', 'Limpieza profunda de dientes', 50.00),
+('Blaqueamiento', 'Tratamiento para balnquear los dientes', 150.00);
