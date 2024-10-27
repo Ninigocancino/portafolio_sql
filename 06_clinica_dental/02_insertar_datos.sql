22b8d906b1dc254eb1f98808e8b75192290e3e31
VictorCancino <juanvictorcancino@gmail.com>
2024-10-26 20:06:47 -0600

    Se agregarón datos a la tabla 'pacientes'

---
+++ 06_clinica_dental/02_insertar_datos.sql
@@ -0,0 +1,4 @@
+--Insertar datos de pacientes
+INSERT INTO pacientes(nombre,fecha_nacimiento,telefono,email)
+VALUES ('Laura Martinez', '1985-06-15', '555-9876','laura@example.com'),
+('José Ramírez', '1990-09-22','555-6543','jose@example.com');
