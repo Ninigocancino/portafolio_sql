9879cb113ff21b00676a12ffb32997b997511d59
VictorCancino <juanvictorcancino@gmail.com>
2024-10-26 19:32:38 -0600

    Se agregarón datos a la tabla habitaciones

---
+++ 04_reservas_estrella_de_Mar/02_insertar_datos.sql
@@ -0,0 +1,5 @@
+--Insertar habitaciones 
+INSERT INTO habitaciones(numero_habitacion, tipo, precio_noche,estado)
+VALUES (101, 'Sencilla', 50.00, 'Diponible'),
+(102, 'Doble', 80.00,'Disponible'),
+(103, 'Suite',150.00, 'Disponible');
